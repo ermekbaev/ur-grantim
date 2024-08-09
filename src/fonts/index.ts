0d1a751fdd0a058c54b5e3c885c5cc0874fe637b
Nikita Naleikin <nnaleikin361@gmail.com>
2024-08-09 13:55:44 +0400

    Fixes

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -2,9 +2,9 @@ import localFont from 'next/font/local';
 
 export const labGrotesqueFont = localFont({
   src: [
-    { path: './files/labgrotesque-regular.woff2', weight: '400' },
-    { path: './files/labgrotesque-medium.woff2', weight: '500' },
-    { path: './files/labgrotesque-bold.woff2', weight: '700' },
+    { path: './files/lab-grotesque-regular.woff2', weight: '400' },
+    { path: './files/lab-grotesque-medium.woff2', weight: '500' },
+    { path: './files/lab-grotesque-bold.woff2', weight: '700' },
   ],
 
   style: 'normal',
